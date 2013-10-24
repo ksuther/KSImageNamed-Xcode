ae47c684a3e37eae686c668d864e9acb167b52cf
Jack Chen (chendo) <github@chen.do>
2013-10-24 14:01:54 +1100

    Hook into DVTTextCompletionListWindowController to be more efficient

--- KSImageNamed/DVTTextCompletionController+KSImageNamed.m
+++ KSImageNamed/DVTTextCompletionController+KSImageNamed.m
@@ -17,10 +17,6 @@
 + (void)load
 {
     MethodSwizzle(self, @selector(acceptCurrentCompletion), @selector(swizzle_acceptCurrentCompletion));
-    
-    MethodSwizzle(self,
-                  @selector(textViewShouldChangeTextInRange:replacementString:),
-                  @selector(swizzle_textViewShouldChangeTextInRange:replacementString:));
 }
 
 - (BOOL)swizzle_acceptCurrentCompletion
@@ -50,71 +46,4 @@
     
     return success;
 }
-
-- (BOOL)swizzle_textViewShouldChangeTextInRange:(NSRange)arg1 replacementString:(NSString *)replacementString
-{
-    BOOL result = [self swizzle_textViewShouldChangeTextInRange:arg1 replacementString:replacementString];
-    
-    @try {
-        if (replacementString) {
-            id document = [[[self.textView window] windowController] document];
-            id index = [[document performSelector:@selector(workspace)] performSelector:@selector(index)];
-            
-            NSArray *imageCompletions = [[KSImageNamed sharedPlugin] imageCompletionsForIndex:index];
-            
-            NSInteger indexOfString = [imageCompletions indexOfObjectPassingTest:^BOOL(KSImageNamedIndexCompletionItem *item, NSUInteger idx, BOOL *stop) {
-                if ([item.fileName isEqualToString:replacementString]) {
-                    *stop = YES;
-                    return YES;
-                }
-                else {
-                    return NO;
-                }
-            }];
-            
-            NSImage *image = nil;
-            if (indexOfString != NSNotFound) {
-                KSImageNamedIndexCompletionItem *completionItem = imageCompletions[indexOfString];
-                image = [[[NSImage alloc] initWithContentsOfURL:completionItem.imageFileURL] autorelease];
-            }
-            
-            [self showPreviewForImage:image];
-        }
-    }
-    @catch (NSException *exception) {
-        //I'd rather not crash if Xcode chokes on something
-    }
-    
-    return result;
-}
-
-- (void)showPreviewForImage:(NSImage *)image
-{
-    KSImageNamedPreviewWindow *imageWindow = [KSImageNamed sharedPlugin].imageWindow;
-    
-    imageWindow.image = image;
-    
-    if (!image) {
-        [imageWindow orderOut:self];
-    } else {
-        NSRect imgRect = NSMakeRect(0.0, 0.0, image.size.width, image.size.height);
-        
-        NSImageView *imageView = [[[NSImageView alloc] initWithFrame:imgRect] autorelease];
-        imageView.image = image;
-        
-        id currentDVTTextCompletionSession = [self currentSession];
-        id currentDVTTextCompletionListWindowController = [currentDVTTextCompletionSession _listWindowController];
-        NSWindow *completionListWindow = [currentDVTTextCompletionListWindowController window];
-        
-        if ([completionListWindow isVisible] && [[currentDVTTextCompletionListWindowController _selectedCompletionItem] isKindOfClass:[KSImageNamedIndexCompletionItem class]]) {
-            NSRect completionListWindowFrame = completionListWindow ? completionListWindow.frame : NSMakeRect(image.size.width, image.size.height, 0.0, 0.0);
-            
-            [imageWindow setFrameTopRightPoint:NSMakePoint(completionListWindowFrame.origin.x - 1.0,
-                                                          completionListWindowFrame.origin.y + completionListWindowFrame.size.height)];
-            
-            [[NSApp keyWindow] addChildWindow:imageWindow ordered:NSWindowAbove];
-        }
-    }
-}
-
 @end
